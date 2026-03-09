78cd6cd0a6897c369364a304bd5db215a67f2f2d
Liane Nguyen <p.b.l.nguyen@tilburguniversity.edu>
2026-03-10 00:56:40 +0100

    Update load_dataset.R to fit makefile

--- src/data-preparation/load_dataset.R
+++ src/data-preparation/load_dataset.R
@@ -1,3 +1,5 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
 library(dplyr)
 library(ggplot2)
 library(data.table)
@@ -7,9 +9,11 @@ library(tidyverse)
 # Full file link
 #https://drive.google.com/file/d/13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x/view?usp=share_link
 
-install.packages("googledrive")
-
+if (!requireNamespace("googledrive", quietly = TRUE)) {
+  install.packages("googledrive")
+}
 library(googledrive)
+
 file_id <- "13AZqPcwUro0jwsZIv6Q3WXeEn58YD5_x"
 url <- paste0("https://drive.google.com/uc?export=download&id=", file_id)
 
@@ -19,3 +23,6 @@ yelp_business <- read.csv("yelp_academic_dataset_business.csv")
 
 #table(yelp_business$postal_code)
 summary(yelp_business$postal_code) #Already characters don't need to change 
+
+download.file(url, destfile = "../data/yelp_business.csv", mode = "wb")
+yelp_business <- read.csv("../data/yelp_business.csv")
