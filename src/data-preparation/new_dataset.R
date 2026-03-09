f1c1018f9ba7210756ecef901765bf6074e1fa59
Liane Nguyen <p.b.l.nguyen@tilburguniversity.edu>
2026-03-10 00:52:11 +0100

    Update new_dataset.R adapt to makefile

--- src/data-preparation/new_dataset.R
+++ src/data-preparation/new_dataset.R
@@ -1,4 +1,16 @@
-#Extract columns needed for analysis (create a new dataset)
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(knitr)
+library(rmarkdown)
 library(tidyverse)
-columns = c("state","postal_code","city","review_count","name","categories","stars")
+
+# Load data
+yelp_business <- read.csv("../data/yelp_business.csv")
+
+# Extract columns needed for analysis
+columns <- c("state", "postal_code", "city", "review_count", "name", "categories", "stars")
 research_project <- yelp_business %>% select(all_of(columns))
+
+# Save so clean_data.R can access it
+write.csv(research_project, "../gen/temp/research_project.csv", row.names = FALSE)
