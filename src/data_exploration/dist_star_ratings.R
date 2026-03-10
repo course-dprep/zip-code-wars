364721179fe649a3b9f7c056d09beff4a766eb9d
Liane Nguyen <p.b.l.nguyen@tilburguniversity.edu>
2026-03-10 10:57:13 +0100

    Update dist_star_ratings.R

--- src/data_exploration/dist_star_ratings.R
+++ src/data_exploration/dist_star_ratings.R
@@ -1,8 +1,16 @@
 library(ggplot2)
 library(dplyr)
 
+# Load data
+research_project_filtered <- read.csv("../gen/temp/research_project_filtered.csv")
+
 # Distribution of star ratings
-ggplot(research_project_filtered, aes(x = stars)) +
+p <- ggplot(research_project_filtered, aes(x = stars)) +
   geom_histogram(binwidth = 0.25) +
   labs(title = "Distribution of Yelp Star Ratings",
        x = "Stars", y = "Count")
+
+# Save plot output
+ggsave("../gen/output/stars_vs_review_count.pdf", plot = p)
+
+
