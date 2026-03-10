9fdf8f0d58ebd8dfe79d2f03552e2e19d4f49def
Liane Nguyen <p.b.l.nguyen@tilburguniversity.edu>
2026-03-10 01:01:12 +0100

    Update clean_data.R to adapt to makefile

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,1 +1,12 @@
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(knitr)
+library(rmarkdown)
+library(tidyverse)
+
+research_project <- read.csv("../gen/temp/research_project.csv")
 research_project_filtered <- research_project %>% filter(postal_code != "")
+
+# Save filtered dataset
+write.csv(research_project_filtered, "../gen/temp/research_project_filtered.csv", row.names = FALSE)
