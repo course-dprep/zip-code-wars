e1f7a594a286f473bb833e3799cb96136d2303bc
Liane Nguyen <p.b.l.nguyen@tilburguniversity.edu>
2026-03-10 13:30:46 +0100

    Update final_regression.R

--- src/analysis/final_regression.R
+++ src/analysis/final_regression.R
@@ -1,4 +1,15 @@
-ggplot(research_project_regression,aes(x = log_density,y = stars,
-           color = industry_categorized)) + geom_smooth(method = "lm", se = FALSE) +
+library(ggplot2)
+library(dplyr)
+
+# Load data
+research_project_regression <- read.csv("../gen/temp/research_project_regression.csv")
+
+# Interaction between density and industry
+p <- ggplot(research_project_regression, aes(x = log_density, y = stars,
+                                             color = industry_categorized)) +
+  geom_smooth(method = "lm", se = FALSE) +
   labs(title = "Interaction Between Density and Industry",
-    x = "Log Same-Industry Density",y = "Yelp Stars" )
+       x = "Log Same-Industry Density", y = "Yelp Stars")
+
+# Save plot output
+ggsave("../gen/output/final_regression.pdf", plot = p)
